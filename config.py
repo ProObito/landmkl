2dbef6bdedc0cd1e9c1b311a8cedf7f836c23a9b
ProObito <ahmedumaid707@gmail.com>
2025-04-30 21:25:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,24 @@ from os import environ, getenv
 id_pattern = re.compile(r'^.\d+$') 
 
 class Config(object):
-    # Telethon client config
+    # pyro client config
     API_ID    = os.environ.get("API_ID", "20718334")
     API_HASH  = os.environ.get("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7830743177:AAHkVvb0AwI-bDqa7O0JUZdb_tvSdS4E0fA") 
 
-    # Database config
-    DB_NAME = os.environ.get("DB_NAME", "Cluster0")     
-    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://spxsolo:umaid2008@cluster0.7fbux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    PORT = os.environ.get("PORT", "3372")
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","Cluster0")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://obito:umaid2008@cluster0.engyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    PORT = os.environ.get("PORT", "8080")
 
-    # Other configs
+    # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5585016974 7328629001').split()]
-    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'animes_sub_society').split(',')
+    FORCE_SUB = os.environ.get('FORCE_SUB', 'animes_sub_society').split(',')
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002200709110"))
     DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002200709110"))
     OWNER_ID = int(os.environ.get("OWNER_ID", "5585016974"))
-
     # Queue config
     QUEUE_MAXSIZE = int(os.environ.get("QUEUE_MAXSIZE", 100))
 
